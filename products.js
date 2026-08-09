ea71d6fd1ffb6e88f8ded3b820b25a600639a648
SMK Farabi <smkfarabi@gmail.com>
2026-08-09 23:54:09 +0600

    Initial commit

---
+++ products.js
@@ -0,0 +1,13 @@
+/* ==========================================================
+   CROCHET AL-CHEMI - PRODUCT DATABASE
+   ========================================================== */
+
+const PRODUCTS_DATA = [
+ {
+    id: "001",
+    name: "ahionar first product",
+    price: "100",
+    image: "assets/products/chain.jpg",
+    whatsappLink: "https://wa.me/8801989029586?text=Hi%2C%20I%20want%20to%20purchase%20ahionar%20first%20product%20(ID%3A%20001)."
+},
+];
