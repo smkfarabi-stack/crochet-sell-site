05bbbbd2d7ee5cfbe029353ea4f81e45bee002e4
smk Farabi <smkfarabi@gmail.com>
2026-08-10 21:17:58 +0600

    Update products.js

--- products.js
+++ products.js
@@ -3,11 +3,12 @@
    ========================================================== */
 
 const PRODUCTS_DATA = [
-{
-    id: "001",
-    name: "Pouch ",
+  {
+    id: "002",
+    name: "Sunflower charm",
+    category: "Medium",
     price: "150",
-    image: "assets/products/Pouch.jpg",
-    whatsappLink: "https://wa.me/8801989029586?text=Hi%2C%20I%20want%20to%20purchase%20Pouch%20%20(ID%3A%20001)."
-},
+    description: "Medium bag charm",
+    image: "Sunflower_charm.jpg"
+  },
 ];
