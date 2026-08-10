e85f3d9f1584e302a9b0bbe6a7b28aab8c1799da
smk Farabi <smkfarabi@gmail.com>
2026-08-10 21:40:41 +0600

    Update products.js

--- products.js
+++ products.js
@@ -12,4 +12,12 @@ const PRODUCTS_DATA = [
     description: "small pouch",
     image: "Pouch.jpg"
   },
+     {
+    id: "002",
+    name: "Sunflower charm",
+    category: "Medium",
+    price: "160",
+    description: "charm",
+    image: "2.jpg"
+  },
 ];
