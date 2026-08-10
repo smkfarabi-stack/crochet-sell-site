35b14a895d8a20f653e916035455f9609fff1e92
smk Farabi <smkfarabi@gmail.com>
2026-08-10 21:48:17 +0600

    Update products.js

--- products.js
+++ products.js
@@ -1,23 +1,23 @@
-/* ==========================================================
+/* ==========================================================================
    CROCHET AL-CHEMI - PRODUCT DATABASE
-   ========================================================== */
+   ========================================================================== */
 
 const PRODUCTS_DATA = [
-
-     {
+  {
     id: "001",
     name: "Pouch / Drawstring bag",
     category: "Small",
     price: "150",
     description: "small pouch",
-    image: "Pouch.jpg"
+    image: "assets/products/Pouch.jpg"
   },
-     {
+  {
     id: "002",
     name: "Sunflower charm",
     category: "Medium",
     price: "160",
     description: "charm",
-    image: "2.jpg"
-  },
+    image: "assets/products/2.jpg"
+  }
 ];
+
