e2c58a99ce387f023b380c6779f5c0475c1d7740
smk Farabi <smkfarabi@gmail.com>
2026-08-10 19:45:16 +0600

    Update products.js

--- products.js
+++ products.js
@@ -7,7 +7,7 @@ const PRODUCTS_DATA = [
     id: "001",
     name: "Pouch/ Drawstring bag",
     price: "150",
-    image: "assets/products/Pouch.1.jpg , Poch.2.jpg",
+    image: "assets/products/Pouch.1.jpg",
     whatsappLink: "https://wa.me/8801989029586?text=Hi%2C%20I%20want%20to%20purchase%20Pouch%2F%20Drawstring%20bag%20(ID%3A%20001)."
 },
 ];
