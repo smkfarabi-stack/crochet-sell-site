adc929bf7c8211a980ed0ebb026886d5a350f00e
smk Farabi <smkfarabi@gmail.com>
2026-08-14 09:03:17 +0600

    Update products.js

--- products.js
+++ products.js
@@ -27,5 +27,21 @@ const PRODUCTS_DATA = [
     description: "keychain",
     image: "assets/products/3.jpg"
   },
+     {
+    id: "004",
+    name: "Strawberry charm",
+    category: "Small",
+    price: "160",
+    description: "1.5 inches (3 cm) tall and 0.7 inches (2 cm) wide",
+    image: "assets/products/4.jpg"
+  },
+     {
+    id: "005",
+    name: "Daisy keychain",
+    category: "Medium",
+    price: "120",
+    description: "2.5 inches in diameter",
+    image: "assets/products/5.jpg"
+  },
 ];
 
